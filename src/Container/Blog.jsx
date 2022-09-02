26c53a0604d2b34de46a45406649b248d5065a6c
jabborovaz <greatman-98@mail.ru>
2022-09-02 11:59:05 +0500

    install eslint and create container

---
+++ src/Container/Blog.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Blog() {
+  return (
+    <div className="w-screen h-screen">
+      <div className="mt-8">
+        <h2 className="text-xl sm:text-3xl flex justify-center font-bold">
+          Posts
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Blog;
